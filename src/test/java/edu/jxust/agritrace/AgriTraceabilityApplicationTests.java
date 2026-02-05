8591e6181796f60e3e80e01876a8ddb92a956ae1
akitomirai <144654246+akitomirai@users.noreply.github.com>
2026-02-05 19:03:23 +0800

    Initial commit

---
+++ src/test/java/edu/jxust/agritrace/AgriTraceabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.jxust.agritrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriTraceabilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
